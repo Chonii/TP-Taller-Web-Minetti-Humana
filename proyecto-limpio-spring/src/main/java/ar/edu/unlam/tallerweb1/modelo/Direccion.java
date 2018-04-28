630184d1ef03d309d667ed32fa5fae2e84cb8408
Minetti-C <31669218+Minetti-C@users.noreply.github.com>
2018-04-28 20:12:46 -0300

    Se agregan todas las clases restantes

---
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
@@ -0,0 +1,17 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Direccion {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String calle;
+	private String numero;
+	
+	
+}
