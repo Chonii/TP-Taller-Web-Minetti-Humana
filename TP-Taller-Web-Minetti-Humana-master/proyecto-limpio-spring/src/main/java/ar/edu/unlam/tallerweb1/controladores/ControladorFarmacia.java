80f1dbff576b85e3a66ec1ce208f8918374ff601
Minetti-C <31669218+Minetti-C@users.noreply.github.com>
2018-05-05 17:10:33 -0300

    Punto5 terminado

--- TP-Taller-Web-Minetti-Humana-master/proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorFarmacia.java
+++ TP-Taller-Web-Minetti-Humana-master/proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorFarmacia.java
@@ -2,26 +2,62 @@ package ar.edu.unlam.tallerweb1.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-/*
+
+
 @Controller
 public class ControladorFarmacia {
-	@RequestMapping(path = "/punto5/{n1}/{n2}", method = RequestMethod.GET)
-	public ModelAndView vistaCorrecta(
-		@RequestParam String n1,
-		@RequestParam String n2) {
+	
+	@RequestMapping(value = "/punto5/{operador}/{num1}/{num2}", method = RequestMethod.GET)
+	public ModelAndView punto5(
+		@PathVariable String operador,
+		@PathVariable Double num1,
+		@PathVariable Double num2){
 		
-		ModelMap modelo = new ModelMap();
+		Double resultado=0.0;
+		String message = "El primer numero es: " + num1;
+		String message2 = "El segundo numero es: " + num2;
+		String message4 = "No se puede dividir por 0";
+		String message5 = "No se reconoce la operacion ingresada";		
 		
-		String message = "El primer numero es: " + n1;
-		String message2 = "El segundo numero es: " + n2;
-		String result = "El resultado es: " ;
-		modelo.put("1", n1);
-		modelo.put("2", n2);
+		ModelMap modeloPunto5 = new ModelMap();
 		
-		return null;
-	}
-} */
+		switch (operador){
+			case "sumar":
+			resultado=num1+num2;
+			break;
+			
+			case "restar":
+			resultado=num1-num2;
+			break;
+			
+			case "multiplicar":
+			resultado=num1*num2;
+			break;
+			
+			case "dividir":
+				if (num2!=0) {
+					resultado=num1/num2;}
+				else {
+					modeloPunto5.put("errorDiv", message4);
+					return new ModelAndView("errorDivision",modeloPunto5);}			
+			break;
+			
+			default:
+				modeloPunto5.put("errorOp", message5);
+				return new ModelAndView("errorOperador",modeloPunto5);
+		}	
+		
+		String message3 = "El resultado de " + operador +" "+ num1 + " y " + num2 + " es igual a: " + resultado;
+		modeloPunto5.put("n1", message);
+		modeloPunto5.put("n2", message2);
+		modeloPunto5.put("res", message3);
+		
+		return new ModelAndView("punto5",modeloPunto5);
+		
+}
+	// http://localhost:8080/proyecto-limpio-spring/punto5/
+}
